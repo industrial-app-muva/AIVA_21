105312a880a002da50c335fa1adaefc12c276654
Antonio Triguero <antonio.triguero98@gmail.com>
2021-03-20 15:18:45 +0100

    Minor fixes

--- test.py
+++ test.py
@@ -1,25 +1,47 @@
 import unittest
 import requests
 
+from capacitor_detector.detector import CapacitorDetector
+from capacitor_detector.api import CapacitorDetectorAPI
+
 class TestAPI(unittest.TestCase):
     def test_constructor(self):
-        pass
+        detector = CapacitorDetector()
+        # TODO: Read same templates as detector and compare the lists
+        self.assertEqual([], detector.templates)
 
     def test_process_img(self):
-        pass
+        detector = CapacitorDetector()
+        # TODO: Read image and read bboxs of the image.
+        _, bboxs = detector.detect(image)
+        self.assertEqual([], bboxs)
 
 class TestDetector(unittest.TestCase):
     def test_constructor(self):
-        pass
+        api = CapacitorDetectorAPI()
+        # TODO: Read same templates as detector and compare the lists
+        self.assertEqual([], api.detector.templates)
 
     def test_detect(self):
-        pass
+        api = CapacitorDetectorAPI()
+        # TODO: Read image and read bboxs of the image.
+        _, bboxs = api.process_img(image)
+        self.assertEqual([], bboxs)
 
 class TestApp(unittest.TestCase):
     def test_hello_world(self):
         pass
 
-    def test_process_img(self):
+    def test_process_img_gui(self):
+        pass
+
+    def test_allowed_file(self):
+        pass
+
+    def test_parse_file_storage_2_numpy(self):
+        pass
+
+    def test_init_gui(self):
         pass
 
 
