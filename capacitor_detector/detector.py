75202cdc1a67da8d6e8bb8d303e8045248f38265
Antonio Triguero <antonio.triguero98@gmail.com>
2021-03-21 13:34:19 +0100

    Issue 3 - Parallelism adedd

--- capacitor_detector/detector.py
+++ capacitor_detector/detector.py
@@ -1,7 +1,9 @@
 import cv2 as cv
-from tqdm import tqdm
 
 import os
+import concurrent.futures
+
+from tqdm import tqdm
 
 TEMPLATES_PATH = 'capacitor_detector/templates'
 
@@ -15,22 +17,33 @@ class CapacitorDetector:
     def __load_templates():
         return os.listdir(TEMPLATES_PATH)
 
-    def detect(self, img, method=cv.TM_CCOEFF_NORMED, threshold=0.9):
-        result_img, b_boxes = img.copy(), []
+    def __process_image(self, img, template, method=cv.TM_CCOEFF_NORMED, threshold=0.9):
+        h, w = template.shape[0], template.shape[1]
+    
+        res = cv.matchTemplate(img, template, method)
+        min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
 
-        for template_file in tqdm(self.templates):
-            template = cv.imread(f'{TEMPLATES_PATH}/{template_file}', 1)
-            h, w = template.shape[0], template.shape[1]
+        if max_val > threshold:
+            bottom_right = (max_loc[0] + h, max_loc[1] + w)
         
-            res = cv.matchTemplate(img, template, method)
-            min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
+            return [*max_loc, w, h]
+        return None
 
-            if max_val > threshold:
-                top_left = max_loc
-            
-                bottom_right = (top_left[0] + h, top_left[1] + w)
-                cv.rectangle(result_img, top_left, bottom_right, 255, 2)
-            
-                b_boxes.append([*top_left, w, h])
+    def detect(self, img):
+        result_img, bboxes = img.copy(), []
 
-        return result_img, b_boxes
+        with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+            futures_process = [executor.submit(self.__process_image, img, cv.imread(f'{TEMPLATES_PATH}/{template_file}', 1)) 
+                               for template_file in self.templates]
+
+            with tqdm(total=len(futures_process)) as pbar:
+                for future in concurrent.futures.as_completed(futures_process):
+                    bbox = future.result()
+                    if bbox:
+                        bboxes.append(future.result())
+                    pbar.update(1)
+
+        for bbox in bboxes:
+            cv.rectangle(result_img, (bbox[0], bbox[1]), (bbox[0] + bbox[3], bbox[1] + bbox[2]), 255, 2)
+        
+        return result_img, bboxes
